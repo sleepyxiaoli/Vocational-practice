f17575e406b6bce855b97dc72247c540bc23e513
xiaoli <129945496+sleepyxiaoli@users.noreply.github.com>
2023-05-03 22:04:01 +0800

    提交

---
+++ login and register/trans.js
@@ -0,0 +1,8 @@
+var TurnToRegister = () => {
+    document.getElementById('registercard_div').style.display = "block";
+    document.getElementById('logincard_div').style.display = "none";
+}
+var TurnToLogin = () => {
+    document.getElementById('logincard_div').style.display = "block";
+    document.getElementById('registercard_div').style.display = "none";
+}
